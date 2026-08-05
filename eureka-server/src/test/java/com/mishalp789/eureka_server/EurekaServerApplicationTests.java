d59bcb713a8ce5eb1aca38e437c068b4fd5b85e0
muhammed mishal <mishalp789@gmail.com>
2026-08-05 20:30:06 +0530

    Initial project setup

---
+++ eureka-server/src/test/java/com/mishalp789/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishalp789.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
