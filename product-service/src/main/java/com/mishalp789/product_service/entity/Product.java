a0ec4fdb384451aaa91911903c8860a5f57d0985
muhammed mishal <mishalp789@gmail.com>
2026-08-07 14:35:09 +0530

    Create Product entity

---
+++ product-service/src/main/java/com/mishalp789/product_service/entity/Product.java
@@ -0,0 +1,24 @@
+package com.mishalp789.product_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+}
