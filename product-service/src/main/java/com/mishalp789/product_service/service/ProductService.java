915cc66d9ab9640ed6e235cee94019433abe1bab
muhammed mishal <mishalp789@gmail.com>
2026-08-07 14:39:17 +0530

    Added Product service

---
+++ product-service/src/main/java/com/mishalp789/product_service/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.mishalp789.product_service.service;
+
+public interface ProductService {
+}
---
+++ product-service/src/main/java/com/mishalp789/product_service/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.mishalp789.product_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+}
