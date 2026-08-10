59deaaa425ee8e41071af7a94c07b361349a6d0b
muhammed mishal <mishalp789@gmail.com>
2026-08-10 14:20:45 +0530

    Updated config files

--- product-service/src/main/java/com/mishalp789/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/mishalp789/product_service/ProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
